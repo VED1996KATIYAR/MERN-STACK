73fdc3934d922c376acb6a4ba621b4afe120dbba
VED1996KATIYAR <ved750katiyar@gmail.com>
2026-08-11 00:48:25 +0530

    Do it

--- MERN STACK ASSIGNMENT/TASK-12(SOLUTION)/script.js
+++ MERN STACK ASSIGNMENT/TASK-12(SOLUTION)/script.js
@@ -1,9 +1,17 @@
-function calculate() {
-    let principal=document.getElementById("principal").value;
-    let rate=document.getElementById("rate").value;
-    let time=document.getElementById("time").value;
-    let compounding=document.getElementById("Compounding").value;   
+// function calculate() {
+//     let principal=document.getElementById("principal").value;
+//     let rate=document.getElementById("rate").value;
+//     let time=document.getElementById("time").value;
+//     let compounding=document.getElementById("Compounding").value;   
 
-    let Amount=principal * (Math.pow(1+(rate/compounding), compounding * time));
-    document.getElementById("result").innerText = `The compound interest after ${time} years is: ${Amount}`
-}
+//     let Amount=principal * (Math.pow(1+(rate/compounding), compounding * time));
+//     document.getElementById("result").innerText = `The compound interest after ${time} years is: ${Amount}`
+// }
+
+let P=10000;
+let R=10;
+let T=2;
+let N=1;
+
+let A=P*Math.pow((1+(R/N)),(N*T));
+console.log(`The compound interest after ${T} years is: ${A}`);
